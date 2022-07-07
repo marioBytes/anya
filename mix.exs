4fecd419da1d1871d9d051c96cf4f6868e353cd4
marioBytes <mario.bytes92@gmail.com>
2022-07-07 15:44:10 -0600

    Add faker to help generate dummy data

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Anya.MixProject do
       {:phoenix, "~> 1.6.8"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:faker, "~> 0.17"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
